5c49d3f5cf39e910dec2bb72a77aa90306deee65
MyungHwanJeon <myunghwan.jeon.offl@gmail.com>
2023-09-08 19:32:43 +0900

    init

--- docker/run.bash
+++ docker/run.bash
@@ -1,3 +1,3 @@
 xhost +local:
 
-docker run --gpus all -it --env="DISPLAY" --net=host --ipc=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v /:/mydata --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined jmong1994/transpose:ProgressLabeller
+docker run --gpus all -it --env="DISPLAY" --net=host --ipc=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v /:/mydata --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined jmong1994/transpose:ProgressLabeller bash
